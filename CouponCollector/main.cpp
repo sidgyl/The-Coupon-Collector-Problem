affd8d6b60dcc364efa27b1cb573a9470c10063e
Sidharth Goyal <sidharthgoyal@Sidharths-MacBook-Pro.local>
2015-06-21 12:02:47 -0500

    Initial Commit

---
+++ CouponCollector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CouponCollector
+//
+//  Created by Sidharth Goyal on 6/21/15.
+//  Copyright (c) 2015 Sidharth Goyal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
